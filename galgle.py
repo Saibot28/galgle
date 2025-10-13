86d246f9375ac56260a7ea0b61052d17c7640e4c
Saibot28 <tob.princen@gmail.com>
2025-10-13 09:38:29 +0200

    Update galgle.py
    add comments

--- galgle.py
+++ galgle.py
@@ -2,35 +2,35 @@ import pyfiglet
 import os
 import pwinput
 
-word = pwinput.pwinput("Enter the word: ").lower()
-os.system('cls' if os.name == 'nt' else 'clear')
-itr = 0
+word = pwinput.pwinput("Enter the word: ").lower()  # Listens for user input and makes it lowercase
+os.system("cls" if os.name == "nt" else "clear")    # Clears screen; "cls" if it's windows, otherwise "clear"
 wordlength = len(word)
-dotline = "_" * wordlength
+dotline = "_" * wordlength      # Makes a line for the letters to be filled in
 correct = False
-guessedLetters = set()
+guessedLetters = set()          # Creates an empty set for incorrectly guessed letters 
 
 mistakes = 0
 print(f"{wordlength} letters. You can make 7 mistakes.")
 print(dotline)
 
-while correct == False:
+while correct == False:                                 # Main game loop
 
-    guess = input("Guess a letter: ")
-    os.system('cls' if os.name == 'nt' else 'clear')
+    guess = input("Guess a letter: ")                   # Listens for input and
+    os.system('cls' if os.name == 'nt' else 'clear')    # clears screen
     wrong_guess = True
 
     for position in range(0, wordlength):
         currentLetter = word[position]
-        if str(currentLetter) == str(guess):
-            dotline = dotline[:position] + currentLetter + dotline[position+1:]
-            wrong_guess = False
+        if str(currentLetter) == str(guess):                                        # Checks if guessed letter is the word's letter in nth position
+            dotline = dotline[:position] + currentLetter + dotline[position+1:]     # Replaces underscore by correct letter
+            wrong_guess = False                                                     # Updates wrong_guess so no mistakes are counted
 
     if wrong_guess == True:
-        mistakes += 1
-        guessedLetters.add(guess)
+        mistakes += 1                   # Counts mistake
+        guessedLetters.add(guess)       # Adds letter to incorrect guesses
 
 
+# Print results and hangman
     print("")
     print(f"{dotline}   Mistakes: {mistakes}/7")
     if mistakes == 0:
@@ -90,9 +90,11 @@ while correct == False:
         print("|  / \\")
         print("")
 
+# Print guessed letters
     print("Guessed letters: "+", ".join(map(str, guessedLetters)))
     print("")
 
+# Checks win/lose conditions
     if mistakes > 6:
         print()
         print(pyfiglet.figlet_format(" YOU", font="sblood"))
