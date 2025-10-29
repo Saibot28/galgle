71032d88f5f5d886ccee359b5d73b22bc50e6db2
Saibot28 <tob.princen@gmail.com>
2025-10-29 15:26:52 +0100

    Add checks for duplicate and invalid guesses

--- galgle.py
+++ galgle.py
@@ -8,6 +8,7 @@ wordlength = len(word)
 dotline = "_" * wordlength      # Makes a line for the letters to be filled in
 correct = False
 guessedLetters = set()          # Creates an empty set for incorrectly guessed letters 
+allGuesses = set()              # Creates an empty set for all guessed letters
 
 mistakes = 0
 print(f"{wordlength} letters. You can make 7 mistakes.")
@@ -18,16 +19,26 @@ while correct == False:                                 # Main game loop
     guess = input("Guess a letter: ")                   # Listens for input and
     os.system('cls' if os.name == 'nt' else 'clear')    # clears screen
     wrong_guess = True
+    duplicate_letter = False
+    invalid_length = False
 
-    for position in range(0, wordlength):
-        currentLetter = word[position]
-        if str(currentLetter) == str(guess):                                        # Checks if guessed letter is the word's letter in nth position
-            dotline = dotline[:position] + currentLetter + dotline[position+1:]     # Replaces underscore by correct letter
-            wrong_guess = False                                                     # Updates wrong_guess so no mistakes are counted
+    if guess in allGuesses:         # Checks if the user input has been guessed before and
+        duplicate_letter = True         # sets a boolean flag accordingly
+    elif len(guess) != 1:
+        invalid_length = True
+    else:
 
-    if wrong_guess == True:
-        mistakes += 1                   # Counts mistake
-        guessedLetters.add(guess)       # Adds letter to incorrect guesses
+        for position in range(0, wordlength):
+            currentLetter = word[position]
+            if str(currentLetter) == str(guess):                                        # Checks if guessed letter is the word's letter in nth position
+                dotline = dotline[:position] + currentLetter + dotline[position+1:]     # Replaces underscore by correct letter
+                wrong_guess = False                                                     # Updates wrong_guess so no mistakes are counted
+
+        if wrong_guess == True:
+            mistakes += 1                   # Counts mistake
+            guessedLetters.add(guess)       # Adds letter to incorrect guesses
+
+        allGuesses.add(guess)               # Adds letter to all guesses
 
 
 # Print results and hangman
@@ -52,6 +63,10 @@ while correct == False:                                 # Main game loop
 
 # Print guessed letters
     print("Guessed letters: "+", ".join(map(str, guessedLetters)))
+    if duplicate_letter == True:
+        print("You've guessed that before!")
+    if invalid_length == True:
+        print("Please guess one letter.")
     print("")
 
 # Checks win/lose conditions
